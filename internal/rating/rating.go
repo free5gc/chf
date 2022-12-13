d8e7b09cc8cf88b2775e5f7d62c94c6ab45ac8ce
roy19991013 <80-ChienAn@users.noreply.gitlab.nems.cs.nctu.edu.tw>
2022-12-13 06:52:02 +0000

    tidy code

--- internal/rating/rating.go
+++ internal/rating/rating.go
@@ -2,11 +2,18 @@ package rating
 
 import (
 	"math"
+	"strings"
+	"time"
 
+	"github.com/free5gc/TarrifUtil/asn"
 	"github.com/free5gc/TarrifUtil/tarrifType"
 	"github.com/free5gc/chf/internal/logger"
 	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/util/mongoapi"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+const chargingDataColl = "chargingData"
 
 func ServiceUsageRetrieval(serviceUsage tarrifType.ServiceUsageRequest) (tarrifType.ServiceUsageResponse, *models.ProblemDetails, bool) {
 	lastgrantedquota := false
@@ -47,4 +54,103 @@ func ServiceUsageRetrieval(serviceUsage tarrifType.ServiceUsageRequest) (tarrifT
 	}
 
 	return rsp, nil, lastgrantedquota
+}
+
+func BuildServiceUsageRequest(chargingData models.ChargingDataRequest, unitUsage models.MultipleUnitUsage, sessionid int64) tarrifType.ServiceUsageRequest {
+	supi := chargingData.SubscriberIdentifier
+	supiType := strings.Split(supi, "-")[0]
+	var subscriberIdentifier tarrifType.SubscriptionID
+
+	switch supiType {
+	case "imsi":
+		logger.ChargingdataPostLog.Debugf("SUPI: %s", supi)
+		subscriberIdentifier = tarrifType.SubscriptionID{
+			SubscriptionIDType: &tarrifType.SubscriptionIDType{Value: tarrifType.END_USER_IMSI},
+			SubscriptionIDData: asn.UTF8String(supi[5:]),
+		}
+	case "nai":
+		subscriberIdentifier = tarrifType.SubscriptionID{
+			SubscriptionIDType: &tarrifType.SubscriptionIDType{Value: tarrifType.END_USER_NAI},
+			SubscriptionIDData: asn.UTF8String(supi[4:]),
+		}
+	case "gci":
+		subscriberIdentifier = tarrifType.SubscriptionID{
+			SubscriptionIDType: &tarrifType.SubscriptionIDType{Value: tarrifType.END_USER_NAI},
+			SubscriptionIDData: asn.UTF8String(supi[4:]),
+		}
+	case "gli":
+		subscriberIdentifier = tarrifType.SubscriptionID{
+			SubscriptionIDType: &tarrifType.SubscriptionIDType{Value: tarrifType.END_USER_NAI},
+			SubscriptionIDData: asn.UTF8String(supi[4:]),
+		}
+	}
+
+	// Rating for each rating group
+	var totalUsaedUnit uint32
+	for _, useduint := range unitUsage.UsedUnitContainer {
+		if useduint.QuotaManagementIndicator == models.QuotaManagementIndicator_OFFLINE_CHARGING {
+			continue
+		}
+
+		totalUsaedUnit += uint32(useduint.TotalVolume)
+	}
+
+	filter := bson.M{"ueId": chargingData.SubscriberIdentifier}
+	chargingInterface, err := mongoapi.RestfulAPIGetOne(chargingDataColl, filter)
+	if err != nil {
+		logger.ChargingdataPostLog.Errorf("Get quota error: %+v", err)
+	}
+
+	// workaround
+	quota := uint32(0)
+	switch value := chargingInterface["quota"].(type) {
+	case int:
+		quota = uint32(value)
+	case int64:
+		quota = uint32(value)
+	case float64:
+		quota = uint32(value)
+	default:
+		logger.ChargingdataPostLog.Errorf("Get quota error: do not belong to int or float, type:%T", chargingInterface["quota"])
+	}
+	// tarrifInterface := chargingInterface["tarrif"].(map[string]interface{})
+	// rateElementInterface := tarrifInterface["rateElement"].(map[string]interface{})
+	// unitCostInterface := rateElementInterface["unitCost"].(map[string]interface{})
+
+	tarrif := tarrifType.CurrentTariff{
+		// CurrencyCode: uint32(tarrifInterface["currencycode"].(int64)),
+		RateElement: &tarrifType.RateElement{
+			UnitCost: &tarrifType.UnitCost{
+				// Exponent:    int(unitCostInterface["exponent"].(int32)),
+				// ValueDigits: int64(unitCostInterface["valueDigits"].(int64)),
+				Exponent:    int(1),
+				ValueDigits: int64(1),
+			},
+		},
+	}
+
+	ServiceUsageRequest := tarrifType.ServiceUsageRequest{
+		SessionID:      int(sessionid),
+		SubscriptionID: &subscriberIdentifier,
+		ActualTime:     time.Now(),
+		ServiceRating: &tarrifType.ServiceRating{
+			RequestedUnits: uint32(unitUsage.RequestedUnit.TotalVolume),
+			ConsumedUnits:  totalUsaedUnit,
+			RequestSubType: &tarrifType.RequestSubType{
+				Value: tarrifType.REQ_SUBTYPE_RESERVE,
+			},
+			CurrentTariff: &tarrif,
+			MonetaryQuota: quota,
+		},
+	}
+	if quota == 0 {
+		ServiceUsageRequest.ServiceRating.RequestSubType.Value = tarrifType.REQ_SUBTYPE_DEBIT
+	}
+
+	for _, trigger := range chargingData.Triggers {
+		if trigger.TriggerType == models.TriggerType_FINAL {
+			ServiceUsageRequest.ServiceRating.RequestSubType.Value = tarrifType.REQ_SUBTYPE_DEBIT
+		}
+	}
+	return ServiceUsageRequest
 }
