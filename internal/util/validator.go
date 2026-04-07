c0d538fa87c38f30f5734aca21e8fafee50939ce
solar224 <cyc.cs14@nycu.edu.tw>
2026-04-07 08:18:44 +0000

    fix: validator and test conflicts

--- internal/util/validator.go
+++ internal/util/validator.go
@@ -2,10 +2,29 @@ package util
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/free5gc/chf/internal/logger"
 	"github.com/free5gc/openapi"
 	"github.com/free5gc/openapi/models"
 )
+
+func ValidateChargingDataCreateRequest(
+	chargingData models.ChfConvergedChargingChargingDataRequest,
+) *models.ProblemDetails {
+	if chargingData.NfConsumerIdentification == nil {
+		detail := "nFConsumerIdentification is not presented"
+		logger.ChargingdataPostLog.Warnln(detail)
+		return &models.ProblemDetails{
+			Title:  "Malformed request syntax",
+			Status: http.StatusBadRequest,
+			Detail: detail,
+			Cause:  "MANDATORY_IE_MISSING",
+		}
+	}
+
+	return nil
+}
 
 func ValidateOnlineChargingRequestedUnit(
 	chargingData models.ChfConvergedChargingChargingDataRequest,
--- internal/util/validator_test.go
+++ internal/util/validator_test.go
@@ -9,6 +9,55 @@ import (
 	"github.com/free5gc/openapi/models"
 )
 
+func TestValidateChargingDataCreateRequest(t *testing.T) {
+	testCases := []struct {
+		description    string
+		chargingData   models.ChfConvergedChargingChargingDataRequest
+		expectProblem  bool
+		expectedStatus int
+		expectedCause  string
+	}{
+		{
+			description: "TC1: missing nFConsumerIdentification should fail",
+			chargingData: models.ChfConvergedChargingChargingDataRequest{
+				SubscriberIdentifier:     "imsi-208930000000003",
+				ChargingId:               1,
+				InvocationSequenceNumber: 1,
+			},
+			expectProblem:  true,
+			expectedStatus: http.StatusBadRequest,
+			expectedCause:  "MANDATORY_IE_MISSING",
+		},
+		{
+			description: "TC2: present nFConsumerIdentification should pass",
+			chargingData: models.ChfConvergedChargingChargingDataRequest{
+				SubscriberIdentifier: "imsi-208930000000003",
+				ChargingId:           1,
+				NfConsumerIdentification: &models.ChfConvergedChargingNfIdentification{
+					NFName:            "amf",
+					NodeFunctionality: "SMF",
+				},
+				InvocationSequenceNumber: 1,
+			},
+			expectProblem: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			problem := ValidateChargingDataCreateRequest(tc.chargingData)
+			if tc.expectProblem {
+				require.NotNil(t, problem)
+				require.Equal(t, tc.expectedStatus, int(problem.Status))
+				require.Equal(t, tc.expectedCause, problem.Cause)
+				return
+			}
+
+			require.Nil(t, problem)
+		})
+	}
+}
+
 func TestValidateOnlineChargingRequestedUnit(t *testing.T) {
 	testCases := []struct {
 		description    string
